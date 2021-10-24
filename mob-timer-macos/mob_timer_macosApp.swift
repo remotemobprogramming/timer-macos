1b62c8d84405aa25aaf7af3d61f5692695f4bfd4
jochen <jochen.christ@innoq.com>
2021-10-24 10:36:24 +0200

    Initial Commit

---
+++ mob-timer-macos/mob_timer_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mob_timer_macosApp.swift
+//  mob-timer-macos
+//
+//  Created by Jochen Christ on 24.10.21.
+//
+
+import SwiftUI
+
+@main
+struct mob_timer_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
